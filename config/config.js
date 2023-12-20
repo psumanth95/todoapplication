11ed15aa55ea7cd9f1bc03490f28fde3121978b0
Sumanth Perumalla <150210731+psumanth95@users.noreply.github.com>
2023-12-19 22:46:25 -0500

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,7 @@
+  //############DATABASE CRENDITIALS############
+module.exports = {
+  HOST: "database-1.ccmd6j6rmnku.us-east-1.rds.amazonaws.com",
+  USER: "admin",
+  PASSWORD: "password",
+  DB: "sampledatabase"
+};
